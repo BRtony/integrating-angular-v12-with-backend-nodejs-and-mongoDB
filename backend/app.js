aba796961d117429e165b21e2ec76d4cff060db5
BRtony <87392702+BRtony@users.noreply.github.com>
2021-10-14 16:30:03 -0300

    go

---
+++ backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require ('cors');
+const mongoose = require ('mongoose');
+const department_controller = require('./department_controller')
+// import express, { json, urlencoded } from 'express';
+// import cors from 'cors';
+// import mongoose from 'mongoose';
+const meuBancoUrl = 'mongodb+srv://weslley_brilhante:weslley_brilhante@cluster0.hwyx2.mongodb.net/aplicacaoHTTP?retryWrites=true&w=majority';
+const app = express();
+
+mongoose.connect(meuBancoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
+
+app.use('/departments', department_controller)
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta ", 3000);
+});
